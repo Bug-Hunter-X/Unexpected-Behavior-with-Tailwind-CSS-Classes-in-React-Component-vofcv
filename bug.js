073fcea4201dbda9b5b451fc9938d07a198abc9e
BugHunter-X <askzenai@gmail.com>
2025-01-16 17:18:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+//This is the buggy code in the js file
+const myComponent = () => {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-3xl font-bold">My Component</h1>
+      <p className="mt-4">This component is using Tailwind CSS.</p>
+    </div>
+  );
+};
+```
